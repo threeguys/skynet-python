8d81785ddda6427e2cd9e57581244f50d74110f4
threeguys <4969340+threeguys@users.noreply.github.com>
2020-04-14 19:42:59 -0700

    Making a few license updates as well as trying to get it to run on Sagemaker

--- skynet/trumpbot/reddit.py
+++ skynet/trumpbot/reddit.py
@@ -1,3 +1,16 @@
+#  Copyright 2020 Ray Cole
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
 
 import praw
 import os
@@ -5,13 +18,13 @@ import time
 
 from skynet.seq2seq import Generator
 
-def run_reddit_bot(model_name, profile_name):
+def run_reddit_bot(model_name, profile_name, user_agent):
     generator = Generator(model_name)
     replied = set([])
 
     while True:
         try:
-            reddit = praw.Reddit(profile_name, user_agent='linux:skynet.bot-%s:0.0.1a (by b0untk1ll3er)' % profile_name)
+            reddit = praw.Reddit(profile_name, user_agent=user_agent)
             group = reddit.subreddit('SkynetProvingGrounds')
 
             while True:
--- train_sagemaker.py
+++ train_sagemaker.py
@@ -1,25 +1,42 @@
 #!/usr/bin/python
 
-import os
-import json
+#  Copyright 2020 Ray Cole
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+# import os
+# import json
 
 from skynet.seq2seq.trainer import train_rnn
 
-channel_names = json.loads(os.environ['SM_CHANNELS'])
-hyperparameters = json.loads(os.environ['SM_HPS'])
-input_path = os.environ['SM_INPUT_DIR']
-input_config_path = os.environ['SM_INPUT_CONFIG_DIR']
-output_data_path = os.environ['SM_OUTPUT_DATA_DIR']
-model_path = os.environ['SM_MODEL_DIR']
+# channel_names = json.loads(os.environ['SM_CHANNELS'])
+# hyperparameters = json.loads(os.environ['SM_HPS'])
+# input_path = os.environ['SM_INPUT_DIR']
+# input_config_path = os.environ['SM_INPUT_CONFIG_DIR']
+# output_data_path = os.environ['SM_OUTPUT_DATA_DIR']
+# model_path = os.environ['SM_MODEL_DIR']
 
-train_rnn([
-        '--name',   'sagemaker-gru-model',
-        '--batch',  int(hyperparameters['batch-size']),
-        '--buffer', int(hyperparameters['buffer-size']),
-        '--type',   hyperparameters['model-type'],
-        '--dim',    int(hyperparameters['embedding-dim']),
-        '--units',  int(hyperparameters['rnn-units']),
-        '--seq',    int(hyperparameters['seq-length']),
-        '--epochs', int(hyperparameters['epochs']),
-        '--input',  os.path.join(input_path, 'corpus'),
-        '--output', output_data_path])
+# train_rnn([
+#         '--name',   'sagemaker-gru-model',
+#         '--batch',  int(hyperparameters['batch-size']),
+#         '--buffer', int(hyperparameters['buffer-size']),
+#         '--type',   hyperparameters['model-type'],
+#         '--dim',    int(hyperparameters['embedding-dim']),
+#         '--units',  int(hyperparameters['rnn-units']),
+#         '--seq',    int(hyperparameters['seq-length']),
+#         '--epochs', int(hyperparameters['epochs']),
+#         '--input',  os.path.join(input_path, 'corpus'),
+#         '--output', output_data_path])
+
+if __name__ == '__main__':
+        train_rnn()
